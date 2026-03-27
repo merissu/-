cb18c2260c5f7adf0128151e730077d1458c2fee
merissu <sxlhxty@gmail.com>
2026-03-27 13:58:55 +0800

    添加了川雾和晴岚

---
+++ source/merissu/merissu/TenshiRandomWeather/HediffCompProperties_RandomTeleport.cs
@@ -0,0 +1,50 @@
+﻿using RimWorld;
+using Verse;
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace merissu
+{
+    public class HediffCompProperties_KawamuraRandomMove : HediffCompProperties
+    {
+        public int intervalTicks = 300; 
+        public float radius = 5f;     
+
+        public HediffCompProperties_KawamuraRandomMove()
+        {
+            this.compClass = typeof(HediffComp_KawamuraRandomMove);
+        }
+    }
+
+    public class HediffComp_KawamuraRandomMove : HediffComp
+    {
+        public HediffCompProperties_KawamuraRandomMove Props => (HediffCompProperties_KawamuraRandomMove)props;
+
+        public override void CompPostTick(ref float severityAdjustment)
+        {
+            base.CompPostTick(ref severityAdjustment);
+
+            if (Pawn.Spawned && Pawn.Map != null && Pawn.IsHashIntervalTick(Props.intervalTicks))
+            {
+                DoKawamuraMove();
+            }
+        }
+
+        private void DoKawamuraMove()
+        {
+            Map map = Pawn.Map;
+            IntVec3 currentPos = Pawn.Position;
+
+            int numCells = GenRadial.NumCellsInRadius(Props.radius);
+            IntVec3 randomOffset = GenRadial.RadialPattern[Rand.Range(0, numCells)];
+            IntVec3 targetPos = currentPos + randomOffset;
+
+            if (targetPos.InBounds(map) && targetPos.Walkable(map) && targetPos != currentPos)
+            {
+                Pawn.Position = targetPos;
+
+                Pawn.pather?.ResetToCurrentPosition();
+            }
+        }
+    }
+}
---
+++ source/merissu/merissu/TenshiRandomWeather/Hediff_Qinglan.cs
@@ -0,0 +1,28 @@
+﻿using Verse;
+using RimWorld;
+
+namespace merissu
+{
+    public class Hediff_Qinglan : HediffWithComps
+    {
+        public override void PostAdd(DamageInfo? dinfo)
+        {
+            base.PostAdd(dinfo);
+
+            HediffDef fullPowerDef = HediffDef.Named("FullPower");
+
+            Hediff firstHediffOfDef = pawn.health.hediffSet.GetFirstHediffOfDef(fullPowerDef);
+
+            if (firstHediffOfDef == null)
+            {
+                firstHediffOfDef = pawn.health.AddHediff(fullPowerDef);
+            }
+
+            if (firstHediffOfDef != null)
+            {
+                firstHediffOfDef.Severity = Rand.RangeInclusive(1, 5);
+            }
+
+        }
+    }
+}
