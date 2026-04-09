0e47d49d42ae4bf7eeae5dbe8173c82e5f63ab11
merissu <sxlhxty@gmail.com>
2026-04-09 12:14:15 +0800

    绀珠药,植物飘荡

---
+++ source/merissu/merissu/绀珠药/MaintainDeathRefusal.cs
@@ -0,0 +1,78 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+using Verse.Sound;
+
+namespace merissu
+{
+    public class Hediff_Gypsum : Hediff_High
+    {
+        public override void PostTick()
+        {
+            base.PostTick();
+
+            if (pawn.IsHashIntervalTick(30))
+            {
+                MaintainDeathRefusal();
+            }
+        }
+
+        private void MaintainDeathRefusal()
+        {
+            Hediff deathRefusal = pawn.health.hediffSet.GetFirstHediffOfDef(RimWorld.HediffDefOf.DeathRefusal);
+            if (deathRefusal == null)
+            {
+                deathRefusal = pawn.health.AddHediff(RimWorld.HediffDefOf.DeathRefusal);
+            }
+
+            if (deathRefusal.Severity < deathRefusal.def.maxSeverity)
+            {
+                deathRefusal.Severity = deathRefusal.def.maxSeverity;
+            }
+        }
+    }
+
+    [StaticConstructorOnStartup]
+    public static class GypsumPatch
+    {
+        static GypsumPatch()
+        {
+            var harmony = new Harmony("merissu.gypsum.patch");
+            var original = AccessTools.Method(typeof(Pawn_HealthTracker), "PreApplyDamage");
+            var prefix = AccessTools.Method(typeof(Patch_InstantDeath), "Prefix");
+            harmony.Patch(original, new HarmonyMethod(prefix));
+        }
+    }
+
+    public static class Patch_InstantDeath
+    {
+        public static bool Prefix(Pawn ___pawn, ref DamageInfo dinfo, out bool absorbed)
+        {
+            absorbed = false;
+
+            if (!___pawn.Dead && ___pawn.health.hediffSet.HasHediff(HediffDef.Named("Gypsum")))
+            {
+                if (dinfo.Def != null && dinfo.Def.ExternalViolenceFor(___pawn))
+                {
+                    ___pawn.Kill(dinfo);
+
+                    SoundDef biuSound = SoundDef.Named("biu");
+                    if (biuSound != null)
+                    {
+                        biuSound.PlayOneShot(new TargetInfo(___pawn.Position, ___pawn.Map));
+                    }
+
+                    Hediff sickness = ___pawn.health.hediffSet.GetFirstHediffOfDef(RimWorld.HediffDefOf.DeathRefusalSickness);
+                    if (sickness != null)
+                    {
+                        ___pawn.health.RemoveHediff(sickness);
+                    }
+
+                    absorbed = true;
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
