7c84cf13f9e4d397bdb631a81396ded42caf5626
merissu <sxlhxty@gmail.com>
2026-04-09 21:54:02 +0800

    植物光效

---
+++ source/merissu/merissu/Patch_MaterialPool_WindFix.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+using HarmonyLib;
+
+namespace merissu
+{
+    [StaticConstructorOnStartup]
+    public static class ModInit
+    {
+        static ModInit()
+        {
+            var harmony = new Harmony("merissu.rimworld.plantfix");
+            harmony.PatchAll();
+        }
+    }
+    [HarmonyPatch(typeof(MaterialPool), "MatFrom", new Type[] { typeof(MaterialRequest) })]
+    public static class Patch_MaterialPool_WindFix
+    {
+        public static void Postfix(Material __result, MaterialRequest req)
+        {
+            if (__result != null && req.shader != null && req.shader.name == "merissu/RW_Plant_bioluminescence")
+            {
+                WindManager.Notify_PlantMaterialCreated(__result);
+            }
+        }
+    }
+}
