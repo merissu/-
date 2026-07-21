36208f335dce1c351e57b020de8313225af16b79
merissu <sxlhxty@gmail.com>
2026-07-21 22:51:17 +0800

    波动枪连发

---
+++ source/merissu/merissu/月兔波动枪/AttackMode_MoonRabbitBurstSpread.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using Verse;
+
+namespace merissu
+{
+    public class AttackMode_WaveGunBurst5 : WaveGunAttackMode
+    {
+        public override string ModeName => "WaveGunBurst5";
+        protected override string ProjectileDefName => "MindStarMine";
+        protected override string SoundDefName => "udongegun";
+
+        public override int BurstCount => 5;
+        public override int TicksBetweenShots => 5;      
+        public override float WarmupTime => 1f;        
+        public override bool PlaySoundOnEveryShot => true; 
+    }
+    public class CompProperties_UdongeDeathBurst : CompProperties
+    {
+        public ThingDef particleMoteDef;
+        public int particleCount = 10;     
+        public float minSpeed = 8f;     
+        public float maxSpeed = 10f;     
+        public float minScale = 0.01f;
+        public float maxScale = 0.2f;     
+
+        public CompProperties_UdongeDeathBurst()
+        {
+            this.compClass = typeof(Comp_UdongeDeathBurst);
+        }
+    }
+
+    public class Comp_UdongeDeathBurst : ThingComp
+    {
+        public CompProperties_UdongeDeathBurst Props => (CompProperties_UdongeDeathBurst)props;
+
+        public override void PostDestroy(DestroyMode mode, Map previousMap)
+        {
+            base.PostDestroy(mode, previousMap);
+
+            if (previousMap == null) return;
+
+            ThingDef moteDef = Props.particleMoteDef ?? ThingDef.Named("Mote_UdongeParticle");
+            Vector3 spawnPos = parent.DrawPos;
+
+            for (int i = 0; i < Props.particleCount; i++)
+            {
+                float angle = Rand.Range(0f, 360f);
+                float speed = Rand.Range(Props.minSpeed, Props.maxSpeed);
+
+                MoteThrown mote = (MoteThrown)ThingMaker.MakeThing(moteDef);
+                mote.exactPosition = spawnPos;
+                mote.exactRotation = Rand.Range(0f, 360f);
+                mote.Scale = Rand.Range(Props.minScale, Props.maxScale);
+                mote.SetVelocity(angle, speed); 
+                mote.rotationRate = Rand.Range(-180f, 180f); 
+
+                GenSpawn.Spawn(mote, spawnPos.ToIntVec3(), previousMap);
+            }
+        }
+    }
+}
---
+++ source/merissu/merissu/月兔波动枪/Verb_MoonRabbitRandomShoot.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Verse;
+
+namespace merissu
+{
+    public abstract class WaveGunAttackMode
+    {
+        public abstract string ModeName { get; }
+        protected abstract string ProjectileDefName { get; }
+        protected abstract string SoundDefName { get; }
+        public abstract int BurstCount { get; }
+        public abstract int TicksBetweenShots { get; }
+        public abstract float WarmupTime { get; }
+        public virtual bool PlaySoundOnEveryShot => true;
+
+        public virtual ThingDef ProjectileDef
+        {
+            get
+            {
+                ThingDef def = ThingDef.Named(ProjectileDefName);
+                if (def == null)
+                {
+                    Log.Error($"[merissu] 无法找到子弹定义: {ProjectileDefName}");
+                }
+                return def;
+            }
+        }
+
+        public virtual SoundDef CastSound => SoundDef.Named(SoundDefName);
+
+        public virtual void OnWarmupStart(Verb_WaveGunRandomShoot verb, LocalTargetInfo target) { }
+        public virtual bool OverrideCastShot(Verb_WaveGunRandomShoot verb, LocalTargetInfo target) => false;
+        public virtual void OnCastShot(Verb_WaveGunRandomShoot verb, LocalTargetInfo target) { }
+        public virtual LocalTargetInfo? GetAdjustedTarget(Verb_WaveGunRandomShoot verb, LocalTargetInfo target, int shotIndex) => null;
+    }
+
+    public class Verb_WaveGunRandomShoot : Verb_Shoot
+    {
+        private static List<WaveGunAttackMode> availableModes;
+        private static FieldInfo cachedBurstField = typeof(Verb).GetField("cachedBurstShotCount", BindingFlags.NonPublic | BindingFlags.Instance);
+        private static FieldInfo cachedTicksField = typeof(Verb).GetField("cachedTicksBetweenBurstShots", BindingFlags.NonPublic | BindingFlags.Instance);
+
+        private WaveGunAttackMode currentMode;
+        private bool isVerbPropsCloned = false;
+        private List<WaveGunAttackMode> drawPile = new List<WaveGunAttackMode>();
+
+        private void InitializeModesIfNeed()
+        {
+            if (availableModes == null)
+            {
+                availableModes = new List<WaveGunAttackMode>
+                {
+                    new AttackMode_WaveGunBurst5() //这里添加模式
+                };
+            }
+        }
+
+        private void CloneVerbPropsIfNeed()
+        {
+            if (!isVerbPropsCloned)
+            {
+                VerbProperties privateProps = new VerbProperties();
+                FieldInfo[] fields = typeof(VerbProperties).GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+                foreach (FieldInfo field in fields)
+                {
+                    field.SetValue(privateProps, field.GetValue(this.verbProps));
+                }
+                this.verbProps = privateProps;
+                isVerbPropsCloned = true;
+            }
+        }
+
+        public override ThingDef Projectile
+        {
+            get
+            {
+                if (currentMode != null) return currentMode.ProjectileDef;
+                return base.Projectile;
+            }
+        }
+
+        public override bool TryStartCastOn(
+            LocalTargetInfo castTarg,
+            LocalTargetInfo destTarg,
+            bool surpriseAttack = false,
+            bool canHitNonTargetPawns = true,
+            bool preventFriendlyFire = false,
+            bool nonInterruptingSelfCast = false)
+        {
+            InitializeModesIfNeed();
+            CloneVerbPropsIfNeed();
+
+            if (availableModes.Count > 0)
+            {
+                if (drawPile.Count == 0)
+                {
+                    drawPile.AddRange(availableModes);
+                    for (int i = 0; i < drawPile.Count; i++)
+                    {
+                        int swapIndex = Rand.Range(i, drawPile.Count);
+                        var temp = drawPile[i];
+                        drawPile[i] = drawPile[swapIndex];
+                        drawPile[swapIndex] = temp;
+                    }
+
+                    if (drawPile.Count > 1 && currentMode != null && drawPile[0] == currentMode)
+                    {
+                        var temp = drawPile[0];
+                        drawPile[0] = drawPile[1];
+                        drawPile[1] = temp;
+                    }
+                }
+
+                currentMode = drawPile[0];
+                drawPile.RemoveAt(0);
+            }
+
+            if (currentMode != null)
+            {
+                verbProps.warmupTime = currentMode.WarmupTime;
+                verbProps.burstShotCount = currentMode.BurstCount;
+                verbProps.ticksBetweenBurstShots = currentMode.TicksBetweenShots;
+                verbProps.soundCast = currentMode.PlaySoundOnEveryShot ? currentMode.CastSound : null;
+
+                cachedBurstField?.SetValue(this, null);
+                cachedTicksField?.SetValue(this, null);
+
+                currentMode.OnWarmupStart(this, castTarg);
+            }
+
+            return base.TryStartCastOn(
+                castTarg, destTarg, surpriseAttack,
+                canHitNonTargetPawns, preventFriendlyFire,
+                nonInterruptingSelfCast);
+        }
+
+        protected override bool TryCastShot()
+        {
+            if (currentMode != null && currentMode.OverrideCastShot(this, this.currentTarget))
+            {
+                currentMode.OnCastShot(this, this.currentTarget);
+                return true;
+            }
+
+            LocalTargetInfo originalTarget = this.currentTarget;
+            LocalTargetInfo? adjustedTarget = null;
+            if (currentMode != null)
+            {
+                adjustedTarget = currentMode.GetAdjustedTarget(this, originalTarget, 0);
+            }
+
+            if (adjustedTarget.HasValue)
+            {
+                this.currentTarget = adjustedTarget.Value;
+            }
+
+            bool success = base.TryCastShot();
+
+            if (adjustedTarget.HasValue)
+            {
+                this.currentTarget = originalTarget;
+            }
+
+            if (success && currentMode != null)
+            {
+                currentMode.OnCastShot(this, originalTarget);
+            }
+
+            return success;
+        }
+    }
+}
